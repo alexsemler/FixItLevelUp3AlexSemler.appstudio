6bdc566d16f987115fd71910eb9992f84f4e8118
alexsemler <alex.semler03@gmail.com>
2019-09-27 14:37:01 -0500

    Initial commit

---
+++ forms/Main/Main.js
@@ -0,0 +1,15 @@
+def carLoanPayment(num1, num2, num3) {
+}
+def homeLoanPayment(num1, num2, num3) {
+}
+
+let choice = prompt("Which calculator do you want to use? Car? Home? (use all lowercase)")
+
+
+if (choice == "car") {
+}
+if (choice == "house") {
+}
+if (choice == "stop") {
+}
+
